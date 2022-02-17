87e62fa31b954068fa9ca09d2b09f5096d008075
dongko <dhkim931101@naver.com>
2022-02-17 21:57:23 +0900

    [인프런] - 미로의 최단거리 탐색

---
+++ src/dongko/inflearn/dfsAndBfsUse_08/MazeShortestPath_11.java
@@ -0,0 +1,61 @@
+package dongko.inflearn.dfsAndBfsUse_08;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+/**
+ * @title 미로의 최단거리 통로
+ * @author dongko
+ * @date 2022. 2. 17.
+ * @link 
+ */
+public class MazeShortestPath_11 {
+	public static class Coordinate {
+		public int x;
+		public int y;
+		
+		public Coordinate(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+	}
+	
+	static int [][] arr = new int [7][7];
+	static int [][] dis = new int [7][7];
+	static Queue<Coordinate> q = new LinkedList<> ();
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr[i].length; j++) {
+				arr[i][j] = in.nextInt();
+			}
+		}
+		
+		arr[0][0] = 1;
+		q.offer(new Coordinate(0, 0));
+		bfs(0, 0);
+		
+		System.out.println(dis[6][6]);
+	}
+	static int [] dx = {-1, 0, 1, 0};
+	static int [] dy = {0, 1, 0, -1};
+	private static void bfs(int x, int y) {
+		while(!q.isEmpty()) {
+			Coordinate curC = q.poll();
+			if(curC.x == 6 && curC.y ==6) break;
+			for(int i = 0; i < 4; i++) {
+				int nx = curC.x+dx[i];
+				int ny = curC.y+dy[i];
+				
+				if(0 <= nx && nx <= 6 && 0 <= ny && ny <= 6 && arr[nx][ny] == 0) {
+					arr[nx][ny] = 1;
+					dis[nx][ny] = dis[curC.x][curC.y] + 1;
+					q.offer(new Coordinate(nx, ny));
+				}
+			}
+		}
+	}
+	
+}
--- src/dongko/inflearn/dfsAndBfsUse_08/Mazeexploration_10.java
+++ src/dongko/inflearn/dfsAndBfsUse_08/Mazeexploration_10.java
@@ -6,21 +6,22 @@ import java.util.Scanner;
  * @title 미로탐색
  * @author dongko
  * @date 2022. 2. 9.
- * @link https://cote.inflearn.com/contest/10/problem/08-10
+ * @link
  */
+@SuppressWarnings("resource")
 public class Mazeexploration_10 {
-	static int [][] arr = new int [8][8];
+	static int [][] arr = new int [7][7];
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
-		for(int i = 1; i <= 7; i++) {
-			for(int j = 1; j <= 7; j++) {
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr[i].length; j++) {
 				arr[i][j] = in.nextInt();
 			}
 		}
 		
-		arr[1][1] = 1;
-		dfs(1,1);
+		arr[0][0] = 1;
+		dfs(0,0);
 		System.out.println(cnt);
 		return;
 	}
@@ -29,14 +30,14 @@ public class Mazeexploration_10 {
 	static int [] dy = {0, 1, 0, -1};
 	static int cnt = 0;
 	private static void dfs(int i, int j) {
-		if(i == 7 && j == 7) {
+		if(i == 6 && j ==6) {
 			cnt++;
 		} else {
-			for(int x = 0; x < 4; x++) {
+			for(int x = 0; x < dx.length; x++) {
 				int newX = i+dx[x];
 				int newY = j+dy[x];
 				
-				if( newX >= 1 && newY >= 1 && newX <= 7 && newY <= 7 && arr[newX][newY] == 0) {
+				if( (newX >= 0 && newY >= 0) && (newX <= 6 && newY <= 6) && arr[newX][newY] != 1) {
 					arr[newX][newY] = 1;
 					dfs(newX, newY);
 					arr[newX][newY] = 0;
